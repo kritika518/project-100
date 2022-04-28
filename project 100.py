90660cbd14d6422376533eed672521629a2275cc
kritika518 <86811030+kritika518@users.noreply.github.com>
2022-04-28 18:26:56 -0500

    Add files via upload

---
+++ project 100.py
@@ -0,0 +1,8 @@
+class Atm:
+def _int_(self,cardnumber,pin)
+self.carnumber = cardnumber
+self.pin = pin
+
+def check_balance(self):
+    print("Your balance is 10000")
+new_user = Atm(Card_number, oin_number)
